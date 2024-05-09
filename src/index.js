b76496fc9aec8b5f5337d544618febf9a48066c2
Илья <meowingless@mail.ru>
2024-05-10 00:48:51 +0300

    hashrouter

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename='/#/'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
